f8a5ba9dc3d322e9e05599db7b193ef2e289b1cb
Mykyta Shishov <workmailnikita@gmail.com>
2020-07-12 21:58:47 +0300

    first commit

---
+++ src/ua/nikita/webchat/web/WebChat.java
@@ -0,0 +1,24 @@
+package ua.nikita.webchat.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WebChat extends HttpServlet {
+
+	private static final long serialVersionUID = -1122966304918363177L;
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+}
